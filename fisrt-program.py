797880a18cb3e2be1a1938ca0af8e3115958f7ee
Lucas Monteiro Inacio <lmsupport@outlook.com>
2020-07-04 08:54:36 -0300

    feat: first program using comments and inputs

---
+++ fisrt-program.py
@@ -0,0 +1,11 @@
+# Comments in python
+
+print('Hello World')
+print('What is your name?') #ask the name
+myName = input()
+print('Nice to meet you, ' + myName)
+print('The lenght of your name is: ')
+print(len(myName))
+print('What is your favorite color?') #ask favorite color
+favoriteColor = input()
+print(favoriteColor + ' is a good color, I like too')

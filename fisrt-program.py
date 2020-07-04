e84266ca4d7859ac47a45d18bf6c982c07ce079f
Lucas Monteiro Inacio <lmsupport@outlook.com>
2020-07-04 09:01:36 -0300

    refactor: age question

--- fisrt-program.py
+++ fisrt-program.py
@@ -1,11 +1,17 @@
 # Comments in python
 
 print('Hello World')
+
 print('What is your name?') #ask the name
 myName = input()
 print('Nice to meet you, ' + myName)
 print('The lenght of your name is: ')
 print(len(myName))
+
+print('What is your age?')
+myAge = input()
+print('Good, you be ' + str(int(myAge)) + 'in 1 year')
+
 print('What is your favorite color?') #ask favorite color
 favoriteColor = input()
 print(favoriteColor + ' is a good color, I like too')

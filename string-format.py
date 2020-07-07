8112398b27dd3ea0cfd7ed4b5d288feb4430ca33
Lucas Monteiro Inacio <lmsupport@outlook.com>
2020-07-06 23:59:16 -0300

    feat: string formats

---
+++ string-format.py
@@ -0,0 +1,8 @@
+print('Hello\nWhat is your name?')
+name = input()
+print('What is your age?')
+age = input()
+print('Where you from?')
+location = input()
+
+print('Hello %s good to see you, you have %s old and is from %s' % (name, age, location))

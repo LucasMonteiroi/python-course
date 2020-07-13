5f446018c7758949002aec5714a0607c92c435ff
Lucas Monteiro Inacio <lmsupport@outlook.com>
2020-07-13 11:26:22 -0300

    refactor: expose dolar value in BRL currency

--- exercises/webscrapper.py
+++ exercises/webscrapper.py
@@ -2,4 +2,5 @@ from selenium import webdriver
 browser = webdriver.Firefox(executable_path = '/usr/local/bin/geckodriver')
 browser.get('https://dolarhoje.com/')
 elem = browser.find_element_by_css_selector('#nacional')
-print(elem)
+print(elem.get_attribute('value'))
+browser.quit()
